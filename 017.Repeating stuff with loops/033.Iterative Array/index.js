5a8d003dcf2edd7751cd08c837039f6288ab1f85
Jayasree1006 <159425361+Jayasree1006@users.noreply.github.com>
2024-03-22 15:41:33 +0800

    Add files via upload

---
+++ 017.Repeating stuff with loops/033.Iterative Array/index.js
@@ -0,0 +1,8 @@
+
+/*I've provided you with an array of strings called people. Loop over the people array with a for loop, printing out each name in uppercase letters. */
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; 
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].toUpperCase());
+}
