238c042debe7becd4253dadbbec3709263bb3d1e
Jayasree1006 <jayasreejai1006@gmail.com>
2024-03-25 16:25:12 +0530

    master

--- 020.Callback and Array methods/index.js
+++ 020.Callback and Array methods/index.js
@@ -1,10 +1,10 @@
-/*const number=[1,2,3,4,5,6]
+const number=[1,2,3,4,5,6]
 function print(element){
     console.log(element)
 }
 print(number[0])
 print(0)
-*/
+
 //for each method
 /*const number=[1,2,3,4,5,6]
 function print(element){
@@ -28,9 +28,28 @@ return x+y;}
 
 add(2,4)
 console.log(add)
-*/
+
 const square = x =>{
     return x*x
 }
 console.log(square)
 
+const number=[1,2,3,4,5,6]
+const caps = number.map(function(num)
+{
+    return num*2;
+})
+console.log(caps)
+
+const add = function(x,y)
+{
+return x+y;}
+let x = add(2,3);
+console.log(x);
+
+const square = x =>{
+    return x*x
+}
+let subject = square(3);
+console.log(subject);
+console.log(square(4));
